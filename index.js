9bbd92d0ba2f5be3ab7e820d3a5153b456efe82f
isitha22 <141574812+isitha22@users.noreply.github.com>
2023-11-01 22:44:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('ABHISHEK-SER', {
+say(' Miraculous ladybug', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`'${name}' By @ABHISHEK SURESH._`, {
+say(`'${name}' By @Miraculous ladybug._`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
